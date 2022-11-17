e8c5d8b57f2ecba3a91386ca2b121fc2c0443187
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-11-17 15:17:54 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  name:'App',
+  components:{
+    AppHeader,
+  }
+}
+</script>
+<template>
+  <AppHeader/>
+</template>
+
+
+
+<style lang="scss">
+@use './components/styles/general.scss';
+
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        name:'AppHeader'
+    
+    }
+</script>
+<template>
+  <header>
+    <h1>ciao</h1>
+  </header>
+</template>
+
+
+<style lang="scss" scoped>
+
+
+
+</style>
